c06216404dec4f9857647047a09941f292c225fe
Anastas-yia <vredina2005a@gmail.com>
2024-11-16 17:08:38 +0300

    Add files via upload
    По совету преподавателя включила еще проверку того, что объект содержит необходимый атрибут. Поэтому вывод в консоль содержит еще одну строку "Волк не может съесть Слон"

---
+++ module_6_1.py
@@ -0,0 +1,53 @@
+
+class Animal:
+    def __init__(self, name):
+        self.name = name
+        self.alive = True # живой
+        self.fed = False # накормленный
+
+    def eat(self, food):
+        if hasattr(food, 'edible'):
+            if food.edible:
+                print(f'{self.name} съел {food.name}')
+                self.fed = True
+            else:
+                print(f'{self.name} не стал есть {food.name}')
+                self.alive = False
+        else:
+            print(f'{self.name} не может съесть {food.name}')
+
+
+class Plant:
+    edible = False # съедобность
+    def __init__(self, name):
+        self.name = name
+
+class Mammal(Animal):
+    pass
+
+class Predator(Animal):
+    pass
+
+class Flower(Plant):
+    pass
+
+class Fruit(Plant):
+    edible = True # переопределяется при наследовании
+
+
+a1 = Predator('Волк')
+a2 = Mammal('Слон')
+p1 = Flower('Тюльпан')
+p2 = Fruit('Банан')
+
+
+print(a1.name)
+print(p1.name)
+
+print(a1.alive)
+print(a2.fed)
+a1.eat(p1)
+a2.eat(p2)
+a1.eat(a2)
+print(a1.alive)
+print(a2.fed)
